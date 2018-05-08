623892dfda771baded786ff6959f260a03d2981a
Vy Bui <01bui@cardinalmail.cua.edu>
2018-05-07 22:30:50 -0400

    commit

--- cpp/gaussian.cpp
+++ cpp/gaussian.cpp
@@ -1,31 +1,5 @@
 #include <math.h>       /* sqrt, exp */
-
-// class declaration
-class Gaussian
-{
-	private:
-		float mu, sigma2;
-
-	public:
-		
-		// constructor functions
-		Gaussian ();
-		Gaussian (float, float);
-
-		// change value of average and standard deviation 
-		void setMu(float);
-		void setSigma2(float);
-
-		// output value of average and standard deviation
-		float getMu();
-		float getSigma2();
-
-		// functions to evaluate 
-		float evaluate (float);
-		Gaussian mul (Gaussian);
-		Gaussian add (Gaussian);
-};
-
+#include "gaussian.h"
 
 Gaussian::Gaussian() {
 	mu = 0;
